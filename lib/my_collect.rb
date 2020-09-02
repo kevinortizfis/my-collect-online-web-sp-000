9b0e9471ff0c7d676b57d5eb7b35796944280381
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-02 23:43:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect(array)
+end
 
-
+#my_collect
+  #can handle an empty collection (FAILED - 1)
+  #yields the correct element from a given collection, in this case languages (FAILED - 2)
+  #returns a new collection of appropriately modified elements, in this case capitalized languages (FAILED - 3)
+  #does not modify the original collection (FAILED - 4)
+  #yields the correct element from the given collection, in this case students (FAILED - 5)
+  #returns a new collection of appropriately modified elements, in this case student first names (FAILED - 6)
+  #does not modify the original collection (FAILED - 7)
